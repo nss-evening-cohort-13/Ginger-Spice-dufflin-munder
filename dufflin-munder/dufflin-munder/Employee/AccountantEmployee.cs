269cc0e86d956140a7ea63df0c1f37d08dd1f3fd
Liz Barnes <embarnes95@gmail.com>
2021-02-08 18:50:43 -0800

    create employee and sales class

---
+++ dufflin-munder/dufflin-munder/Employee/AccountantEmployee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dufflin_munder.Employee
+{
+    class AccountantEmployee : Employee
+    {
+        static public string[] Accountant = { "Oscar", "Kevin" };
+    }
+}
