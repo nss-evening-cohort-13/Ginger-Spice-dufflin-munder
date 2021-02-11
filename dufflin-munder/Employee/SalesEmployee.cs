35d50e32e146a1465d58024872949b476e596fb3
Liz Barnes <embarnes95@gmail.com>
2021-02-11 15:59:57 -0800

    create properties for SaleEmployee Class, create method to print list of Sales Employee options, create method to take in user input and determine selected Sales Agent

--- dufflin-munder/Employee/SalesEmployee.cs
+++ dufflin-munder/Employee/SalesEmployee.cs
@@ -6,5 +6,35 @@ namespace dufflin_munder.Employee
 {
     class SalesEmployee : Employee
     {
+        public static List<string> SalesEmployees = new List<string>() { "Dwight Hyte", "Tim Halbert", "Phyllis Leaf" };
+        // Store user selection of Sales Agent
+        public static string SalesAgentSelection;
+        public static string FirstName;
+
+        public static void SalesEmployeesOptions()
+        {
+            Console.WriteLine("Which Sales Employee Are You?");
+            // Use num to number the list of employee options
+            int num = 1;
+            // Loop through SalesEmployees and print to console
+            foreach (string employee in SalesEmployees)
+            {
+                Console.WriteLine($"{num}. {employee}");
+                num++;
+            }
+        }
+
+        public static void SalesEmployeeSelection()
+        {
+            int userinput = Convert.ToInt32(Console.ReadLine());
+            // Use user input to determine index of employee in List
+            int index = userinput - 1;
+            // Set property to user selection
+            SalesAgentSelection = SalesEmployees[index];
+            // Split full name and set first name to variable in order to greet agent
+            string[] splitName = SalesAgentSelection.Split(" ");
+            FirstName = splitName[0];
+            Console.WriteLine($"Hi, {FirstName}!");
+        }
     }
 }
